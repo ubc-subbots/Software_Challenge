2691aec359887f9380249e23cde792b7043d5d7e
Gareth Ellis <gareth.ellis0@gmail.com>
2018-09-13 09:19:32 -0700

    Removed Extra Call To Rosdep

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -23,10 +23,6 @@ CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Install all required dependencies to build this repo
 rosdep install --from-paths src --ignore-src --rosdistro melodic -y
 
-# We have this extra rosdep call because rosdep can't seem to find nested
-# `package.xml` files, like those in src/gps_umd/gps_common
-rosdep install --from-paths --rosdistro melodic -y
-
 echo "================================================================"
 echo "Finished installing other ROS dependencies."
 echo "================================================================"
